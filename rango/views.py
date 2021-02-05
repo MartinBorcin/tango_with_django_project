335ae07cfc554009997f7b7bc5f8dee882481548
Martin Borcin <2481234b@student.gla.ac.uk>
2021-02-05 13:32:30 +0100

    fixup! 3.6 added an about page

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! Go ahead and visit the <a href=/rango/about> about page </a>.")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
 
 def about(request):
-    return HttpResponse("Rango says this is the about page. <br/> <a href=/> <- Go Back Home </a>")
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
