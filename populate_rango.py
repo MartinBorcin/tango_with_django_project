87d2039cd8b7146f525621c6f3a98a8ae4f66ff0
Martin Borcin <2481234b@student.gla.ac.uk>
2021-02-10 16:25:41 +0100

    5.8 updated models and changed the admin interface

--- populate_rango.py
+++ populate_rango.py
@@ -44,9 +44,9 @@ def add_page(cat, title, url, views=0):
     p.save()
 
 
-def add_cat(name, views, likes):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
-    c.likes = views
+    c.views = views
     c.likes = likes
     c.save()
     return c
